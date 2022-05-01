98623a84d15eefcb071547c27a37c927130ff1c7
kledson CR <kledson.rabelo.pb@compasso.com.br>
2022-05-01 19:07:09 -0300

    feat: added método Post

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,9 @@
 module.exports = app => {
     app.get('/api/v1/user', (req, res) => res.send('Realizando GET na rota usuários!'))
+
+    app.post('/api/v1/user', (req, res) => {
+        console.log(req.body)
+        res.send('Realizando POST na rota usuários!')
+    })
+
 }
