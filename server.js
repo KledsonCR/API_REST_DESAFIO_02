eaa91be95dc8f12c92923b51d68dfa4f6a29090b
kledson CR <kledson.rabelo.pb@compasso.com.br>
2022-05-01 17:32:54 -0300

    feat: added folder config e atualizando dados no customExpress

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const consign = require('consign')
+const customExpress = require('./config/customExpress')
 
-const app = express()
+const app = customExpress()
 
 app.listen(3000, () => console.log('api em execução!'))
+
 
