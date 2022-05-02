d24c48b8fb63ae7d51d15b745659aeb843daa200
kledson CR <kledson.rabelo.pb@compasso.com.br>
2022-05-02 01:18:02 -0300

    feat: importando Tabelas no server

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const customExpress = require('./config/customExpress')
 const conexao = require('./data/conexao')
+const Tabelas = require('./data/Tabelas')
 
 conexao.connect(erro => {
    if(erro) {
        console.log(erro)
    } else {
        console.log('conectado com sucesso!')
-
+    
+       Tabelas.init(conexao)
        const app = customExpress()
 
        app.listen(3000, () => console.log('api em execução!'))
