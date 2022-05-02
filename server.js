03e8c003b5cffcf91636bd5a18239d7675f98cc5
kledson CR <kledson.rabelo.pb@compasso.com.br>
2022-05-01 23:59:57 -0300

    feat: importando conexao no server

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const customExpress = require('./config/customExpress')
+const conexao = require('./data/conexao')
 
-const app = customExpress()
+conexao.connect(erro => {
+   if(erro) {
+       console.log(erro)
+   } else {
+       console.log('conectado com sucesso!')
 
-app.listen(3000, () => console.log('api em execução!'))
+       const app = customExpress()
+
+       app.listen(3000, () => console.log('api em execução!'))
+   }
+})
+
 
 
