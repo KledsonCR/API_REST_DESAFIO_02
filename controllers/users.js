9732dea3882e65ece9a4776771b08d760c17964c
kledson CR <kledson.rabelo.pb@compasso.com.br>
2022-05-02 16:20:07 -0300

    feat: added métodos para consulta

--- controllers/users.js
+++ controllers/users.js
@@ -1,13 +1,34 @@
 const USERS = require('../models/users')
 
 module.exports = app => {
-    app.get('/api/v1/user', (req, res) => res.send('Realizando GET na rota usuários!'))
+
+    app.get('/api/v1/user', (req, res) => {
+            USERS.lista(res)
+    })
+
+    app.get('/api/v1/user/:id', (req, res) => {
+        const id = parseInt(req.params.id);
+
+        USERS.buscarPorId(id, res);
+    }) 
 
     app.post('/api/v1/user', (req, res) => {
         const users = req.body
 
-        USERS.adiciona(users)
-        res.send('Realizando POST na rota usuários!')
+        USERS.adiciona(users, res)
     })
+
+    app.put('/api/v1/user/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        const valores = req.body
+
+        USERS.altera(id, valores, res);
+    }) 
+
+    app.delete('/api/v1/user/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+
+        USERS.deleta(id, res)
+    }) 
 
 }
