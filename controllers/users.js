cacdefdc76ed0cddf7a8c7233f7bf1e2decef9f3
kledson CR <kledson.rabelo.pb@compasso.com.br>
2022-05-02 18:42:43 -0300

    feat: added comentarios

--- controllers/users.js
+++ controllers/users.js
@@ -1,30 +1,30 @@
 const USERS = require('../models/users')
 
 module.exports = app => {
-
+    //Método GET
     app.get('/api/v1/user', (req, res) => {
             USERS.lista(res)
     })
-
+    //Método GET pegando por Id
     app.get('/api/v1/user/:id', (req, res) => {
         const id = parseInt(req.params.id);
 
         USERS.buscarPorId(id, res);
     }) 
-
+    //Método POST
     app.post('/api/v1/user', (req, res) => {
         const users = req.body
 
         USERS.adiciona(users, res)
     })
-
+    //Método PUT
     app.put('/api/v1/user/:id', (req, res) => {
         const id = parseInt(req.params.id)
         const valores = req.body
 
         USERS.altera(id, valores, res);
     }) 
-
+    //Método DELETE
     app.delete('/api/v1/user/:id', (req, res) => {
         const id = parseInt(req.params.id)
 
