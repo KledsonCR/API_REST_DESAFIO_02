d1ab653c11d7d0f7f0d8a006b41bf1e54d553013
kledson CR <kledson.rabelo.pb@compasso.com.br>
2022-05-02 01:13:19 -0300

    feat: added class para criar tables

---
+++ data/tabelas.js
@@ -0,0 +1,36 @@
+class Tabelas {
+    init(conexao) {
+         this.conexao = conexao
+
+         this.criarUSERS()
+    }
+
+    criarUSERS() {
+         const sql = `
+                        CREATE TABLE IF NOT EXISTS Users (
+                        id int NOT NULL AUTO_INCREMENT,
+                        name varchar(50) NOT NULL,
+                        cpf varchar(11) NOT NULL,
+                        birthDate DATE NOT NULL,
+                        email varchar(40) NOT NULL,
+                        password varchar(30) NOT NULL,
+                        address varchar(200) NOT NULL,
+                        number varchar(15) NOT NULL,
+                        complement varchar(60) NOT NULL,
+                        city varchar(50) NOT NULL,
+                        state varchar(20) NOT NULL,
+                        country varchar(40) NOT NULL,
+                        zipCode varchar(11) NOT NULL,
+                        PRIMARY KEY (id))
+                      `
+         this.conexao.query(sql, (erro) => {
+             if(erro) {
+                 console.log(erro)
+             } else {
+                 console.log('Tabela users criada com sucesso!') 
+             }
+         })
+    }
+}
+
+module.exports = new Tabelas
